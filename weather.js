b776061071ca99f07c166f61950a21d4e068e3cf
Elaheh <elahe.sh76@gmail.com>
2023-04-23 20:30:04 +0430

    resolve merge conflicts with master

--- weather.js
+++ weather.js
@@ -1,65 +1,15 @@
 let day=document.querySelector("#Date")
 let fullDate = new Date();
 let weekDays=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
-<<<<<<< HEAD
-
-day.innerHTML=`${weekDays[fullDate.getDay()]} ${fullDate.getHours()}:${fullDate.getMinutes()}`;
-// console.log(day)
-
-
-
-let city=document.querySelector("#city");
-console.log(city)
-=======
 let Months=['January','February','March','April','May','June','July','August','September','October','November','December']
 day.innerHTML=`${weekDays[fullDate.getDay()]} ${fullDate.getHours()}:${fullDate.getMinutes()}`;
 let city=document.querySelector("#city");
 let celsius=null;
->>>>>>> master
 
 let submit=document.querySelector("form")
 submit.addEventListener("submit",function(event){
   event.preventDefault();
   let inputCity = document.querySelector("#enter-city").value;
-<<<<<<< HEAD
-
-  let input = inputCity.charAt(0).toUpperCase() + inputCity.slice(1);
-  city.innerHTML = input;
-  console.log(input);
-
-  let urlApi = `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=2bd326a60dc89a53287e446e819664df&units=metric`;
-
-  function showTemp(response) {
-    let temp = Math.round(response.data.main.temp);
-    console.log(response.data.main.temp);
-    document.getElementById("Tempreture").innerHTML = temp;
-  }
-  axios.get(urlApi).then(showTemp);
-}
-)
-
-document.getElementById("current").addEventListener("click", function () {
-  function currentTime(position) {
-    console.log(position.coords);
-    let latitude = position.coords.latitude;
-    console.log(latitude);
-    let longitude = position.coords.longitude;
-    console.log(longitude);
-    let urlApi = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=2bd326a60dc89a53287e446e819664df&units=metric`;
-
-axios.get(urlApi).then(showCurrent);
-
-    function showCurrent(response) {
-   let currentCity=response.data.name;
-   let currentTemp = Math.round(response.data.main.temp);
-   document.getElementById("Tempreture").innerHTML = currentTemp;
-   document.getElementById("city").innerHTML = currentCity;
-    }
-    
-  }
-
-  navigator.geolocation.getCurrentPosition(currentTime);
-=======
   let input = inputCity.charAt(0).toUpperCase() + inputCity.slice(1);
   city.innerHTML = input;
   console.log(input);
@@ -143,7 +93,6 @@ document.getElementById("city").innerHTML = currentCity;
 }
 }
 navigator.geolocation.getCurrentPosition(currentTime);
->>>>>>> master
 });
 
 
@@ -153,25 +102,11 @@ navigator.geolocation.getCurrentPosition(currentTime);
 
 
 
-<<<<<<< HEAD
-let temp = document.querySelector("#Tempreture");
-let centigrade = document.querySelector("#centigrade");
-let farenheit = document.querySelector("#farenheit");
-centigrade.addEventListener("click",function(event){
-    event.preventDefault();
-    temp.innerHTML="4"
-})
-farenheit.addEventListener("click", function (event) {
-  event.preventDefault();
-  temp.innerHTML = "66 ";
-});
-=======
 
 
 
 
 
->>>>>>> master
 
 
 
